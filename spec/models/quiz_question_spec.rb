8f87f5a6744dcafcd5fd159594feb95da34401ee
TPan11 <pandetanaysoc@gmail.com>
2016-04-01 11:31:46 -0400

    Added tests for complete and view_completed_question method
    Added tests for complete and view_completed_question method

--- spec/models/quiz_question_spec.rb
+++ spec/models/quiz_question_spec.rb
@@ -241,4 +241,378 @@ describe QuizQuestion do
       end
     end
   end
+
+  describe "view_question_text" do
+    context "when the question is a multiple-choice radio" do
+      it "contains the text of the question" do
+        expect(@radio_question.view_question_text.html_safe).to include("In which city is NCSU located?")
+      end
+
+      it "contains each of the choices" do
+        expect(@radio_question.view_question_text.html_safe).to include("Raleigh")
+        expect(@radio_question.view_question_text.html_safe).to include("Atlanta")
+        expect(@radio_question.view_question_text.html_safe).to include("Charlotte")
+        expect(@radio_question.view_question_text.html_safe).to include("North Carolina")
+      end
+
+      it "bolds the correct answer" do
+        expect(@radio_question.view_question_text.html_safe).to include("<b>Raleigh</b>")
+      end
+
+      it "does not bold incorrect answers" do
+        expect(@radio_question.view_question_text.html_safe).not_to include("<b>Atlanta</b>")
+        expect(@radio_question.view_question_text.html_safe).not_to include("<b>Charlotte</b>")
+        expect(@radio_question.view_question_text.html_safe).not_to include("<b>North Carolina</b>")
+      end
+    end
+
+    context "when the question is a multiple-choice checkbox" do
+      it "contains the text of the question" do
+        expect(@checkbox_question.view_question_text.html_safe).to include("Which of the following are colors?")
+      end
+
+      it "contains each of the choices" do
+        expect(@checkbox_question.view_question_text.html_safe).to include("Paint")
+        expect(@checkbox_question.view_question_text.html_safe).to include("Blue")
+        expect(@checkbox_question.view_question_text.html_safe).to include("Car")
+        expect(@checkbox_question.view_question_text.html_safe).to include("Red")
+      end
+
+      it "bolds the correct answers" do
+        expect(@checkbox_question.view_question_text.html_safe).to include("<b>Blue</b>")
+        expect(@checkbox_question.view_question_text.html_safe).to include("<b>Red</b>")
+      end
+
+      it "does not bold incorrect answers" do
+        expect(@checkbox_question.view_question_text.html_safe).not_to include("<b>Paint</b>")
+        expect(@checkbox_question.view_question_text.html_safe).not_to include("<b>Car</b>")
+      end
+    end
+
+    context "when the question is true/false" do
+      it "contains the text of the question" do
+        expect(@true_false_question.view_question_text.html_safe).to include("2+2=4")
+      end
+
+      it "contains each of the choices" do
+        expect(@true_false_question.view_question_text.html_safe).to include("True")
+        expect(@true_false_question.view_question_text.html_safe).to include("False")
+      end
+
+      it "bolds the correct answer" do
+        expect(@true_false_question.view_question_text.html_safe).to include("<b>True</b>")
+      end
+
+      it "does not bold the incorrect answer" do
+        expect(@true_false_question.view_question_text.html_safe).not_to include("<b>False</b>")
+      end
+    end
+  end
+
+  describe "view_completed_question" do
+    context "when the question is a multiple-choice radio" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@radio_question.edit(0)).to include("In which city is NCSU located?</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@radio_question.edit(0)).to include("Raleigh")
+        expect(@radio_question.edit(0)).to include("Atlanta")
+        expect(@radio_question.edit(0)).to include("Charlotte")
+        expect(@radio_question.edit(0)).to include("North Carolina")
+      end
+
+      it "puts each choice in a radio button" do
+        expect(@radio_question.edit(0).scan('input type="radio"').size).to eq(4)
+      end
+
+      it "selects only the correct answer" do
+        expect(@radio_question.edit(0)).to include('value="1" checked="checked"')
+      end
+
+      it "does not select the incorrect answers" do
+        expect(@radio_question.edit(0)).to include('value="2" />')
+        expect(@radio_question.edit(0)).to include('value="3" />')
+        expect(@radio_question.edit(0)).to include('value="4" />')
+      end
+    end
+
+    context "when the question is a multiple-choice checkbox" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@checkbox_question.edit(0)).to include("Which of the following are colors?</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@checkbox_question.edit(0)).to include("Paint")
+        expect(@checkbox_question.edit(0)).to include("Blue")
+        expect(@checkbox_question.edit(0)).to include("Car")
+        expect(@checkbox_question.edit(0)).to include("Red")
+      end
+
+      it "puts each choice in a check box" do
+        expect(@checkbox_question.edit(0).scan('input type="checkbox"').size).to eq(4)
+      end
+
+      it "checks only the correct answers" do
+        expect(@checkbox_question.edit(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_2_iscorrect" value="1" checked="checked"')
+        expect(@checkbox_question.edit(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_4_iscorrect" value="1" checked="checked"')
+      end
+
+      it "does not check the incorrect answers" do
+        expect(@checkbox_question.edit(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_1_iscorrect" value="1" />')
+        expect(@checkbox_question.edit(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_3_iscorrect" value="1" />')
+      end
+    end
+
+    context "when the question is true/false" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@true_false_question.edit(0)).to include("2+2=4</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@true_false_question.edit(0)).to include("True")
+        expect(@true_false_question.edit(0)).to include("False")
+      end
+
+      it "puts each choice in a radio button" do
+        expect(@true_false_question.edit(0).scan('input type="radio"').size).to eq(2)
+      end
+
+      it "selects only the correct answer" do
+        expect(@true_false_question.edit(0)).to include('value="True" checked="checked"')
+      end
+
+      it "does not select the incorrect answer" do
+        expect(@true_false_question.edit(0)).to include('value="False" />')
+      end
+    end
+  end
+
+  describe "view_question_text" do
+    context "when the question is a multiple-choice radio" do
+      it "contains the text of the question" do
+        expect(@radio_question.view_question_text.html_safe).to include("In which city is NCSU located?")
+      end
+
+      it "contains each of the choices" do
+        expect(@radio_question.view_question_text.html_safe).to include("Raleigh")
+        expect(@radio_question.view_question_text.html_safe).to include("Atlanta")
+        expect(@radio_question.view_question_text.html_safe).to include("Charlotte")
+        expect(@radio_question.view_question_text.html_safe).to include("North Carolina")
+      end
+
+      it "bolds the correct answer" do
+        expect(@radio_question.view_question_text.html_safe).to include("<b>Raleigh</b>")
+      end
+
+      it "does not bold incorrect answers" do
+        expect(@radio_question.view_question_text.html_safe).not_to include("<b>Atlanta</b>")
+        expect(@radio_question.view_question_text.html_safe).not_to include("<b>Charlotte</b>")
+        expect(@radio_question.view_question_text.html_safe).not_to include("<b>North Carolina</b>")
+      end
+    end
+
+    context "when the question is a multiple-choice checkbox" do
+      it "contains the text of the question" do
+        expect(@checkbox_question.view_question_text.html_safe).to include("Which of the following are colors?")
+      end
+
+      it "contains each of the choices" do
+        expect(@checkbox_question.view_question_text.html_safe).to include("Paint")
+        expect(@checkbox_question.view_question_text.html_safe).to include("Blue")
+        expect(@checkbox_question.view_question_text.html_safe).to include("Car")
+        expect(@checkbox_question.view_question_text.html_safe).to include("Red")
+      end
+
+      it "bolds the correct answers" do
+        expect(@checkbox_question.view_question_text.html_safe).to include("<b>Blue</b>")
+        expect(@checkbox_question.view_question_text.html_safe).to include("<b>Red</b>")
+      end
+
+      it "does not bold incorrect answers" do
+        expect(@checkbox_question.view_question_text.html_safe).not_to include("<b>Paint</b>")
+        expect(@checkbox_question.view_question_text.html_safe).not_to include("<b>Car</b>")
+      end
+    end
+
+    context "when the question is true/false" do
+      it "contains the text of the question" do
+        expect(@true_false_question.view_question_text.html_safe).to include("2+2=4")
+      end
+
+      it "contains each of the choices" do
+        expect(@true_false_question.view_question_text.html_safe).to include("True")
+        expect(@true_false_question.view_question_text.html_safe).to include("False")
+      end
+
+      it "bolds the correct answer" do
+        expect(@true_false_question.view_question_text.html_safe).to include("<b>True</b>")
+      end
+
+      it "does not bold the incorrect answer" do
+        expect(@true_false_question.view_question_text.html_safe).not_to include("<b>False</b>")
+      end
+    end
+  end
+
+  describe "complete" do
+    context "when the question is a multiple-choice radio" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@radio_question.complete(0).htmlsafe).to include("In which city is NCSU located?</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@radio_question.complete(0)).to include("Raleigh")
+        expect(@radio_question.complete(0)).to include("Atlanta")
+        expect(@radio_question.complete(0)).to include("Charlotte")
+        expect(@radio_question.complete(0)).to include("North Carolina")
+      end
+
+      it "puts each choice in a radio button" do
+        expect(@radio_question.complete(0).scan('input type="radio"').size).to eq(4)
+      end
+
+      it "selects only the correct answer" do
+        expect(@radio_question.complete(0)).to include('value="1" checked="checked"')
+      end
+
+      it "does not select the incorrect answers" do
+        expect(@radio_question.complete(0)).to include('value="2" />')
+        expect(@radio_question.complete(0)).to include('value="3" />')
+        expect(@radio_question.complete(0)).to include('value="4" />')
+      end
+    end
+
+    context "when the question is a multiple-choice checkbox" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@checkbox_question.complete(0)).to include("Which of the following are colors?</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@checkbox_question.complete(0)).to include("Paint")
+        expect(@checkbox_question.complete(0)).to include("Blue")
+        expect(@checkbox_question.complete(0)).to include("Car")
+        expect(@checkbox_question.complete(0)).to include("Red")
+      end
+
+      it "puts each choice in a check box" do
+        expect(@checkbox_question.complete(0).scan('input type="checkbox"').size).to eq(4)
+      end
+
+      it "checks only the correct answers" do
+        expect(@checkbox_question.complete(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_2_iscorrect" value="1" checked="checked"')
+        expect(@checkbox_question.complete(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_4_iscorrect" value="1" checked="checked"')
+      end
+
+      it "does not check the incorrect answers" do
+        expect(@checkbox_question.complete(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_1_iscorrect" value="1" />')
+        expect(@checkbox_question.complete(0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_3_iscorrect" value="1" />')
+      end
+    end
+
+    context "when the question is true/false" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@true_false_question.complete(0)).to include("2+2=4</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@true_false_question.complete(0)).to include("True")
+        expect(@true_false_question.complete(0)).to include("False")
+      end
+
+      it "puts each choice in a radio button" do
+        expect(@true_false_question.complete(0).scan('input type="radio"').size).to eq(2)
+      end
+
+      it "selects only the correct answer" do
+        expect(@true_false_question.complete(0)).to include('value="True" checked="checked"')
+      end
+
+      it "does not select the incorrect answer" do
+        expect(@true_false_question.complete(0)).to include('value="False" />')
+      end
+    end
+  end
+
+
+  describe "view_completed_question" do
+    context "when the question is a multiple-choice radio" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@radio_question.view_completed_question(0,0).htmlsafe).to include("In which city is NCSU located?</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@radio_question.view_completed_question(0,0)).to include("Raleigh")
+        expect(@radio_question.view_completed_question(0,0)).to include("Atlanta")
+        expect(@radio_question.view_completed_question(0,0)).to include("Charlotte")
+        expect(@radio_question.view_completed_question(0,0)).to include("North Carolina")
+      end
+
+      it "puts each choice in a radio button" do
+        expect(@radio_question.view_completed_question(0,0).scan('input type="radio"').size).to eq(4)
+      end
+
+      it "selects only the correct answer" do
+        expect(@radio_question.view_completed_question(0,0)).to include('value="1" checked="checked"')
+      end
+
+      it "does not select the incorrect answers" do
+        expect(@radio_question.view_completed_question(0,0)).to include('value="2" />')
+        expect(@radio_question.view_completed_question(0,0)).to include('value="3" />')
+        expect(@radio_question.view_completed_question(0,0)).to include('value="4" />')
+      end
+    end
+
+    context "when the question is a multiple-choice checkbox" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@checkbox_question.view_completed_question(0,0)).to include("Which of the following are colors?</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@checkbox_question.view_completed_question(0,0)).to include("Paint")
+        expect(@checkbox_question.view_completed_question(0,0)).to include("Blue")
+        expect(@checkbox_question.view_completed_question(0,0)).to include("Car")
+        expect(@checkbox_question.view_completed_question(0,0)).to include("Red")
+      end
+
+      it "puts each choice in a check box" do
+        expect(@checkbox_question.view_completed_question(0,0).scan('input type="checkbox"').size).to eq(4)
+      end
+
+      it "checks only the correct answers" do
+        expect(@checkbox_question.view_completed_question(0,0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_2_iscorrect" value="1" checked="checked"')
+        expect(@checkbox_question.view_completed_question(0,0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_4_iscorrect" value="1" checked="checked"')
+      end
+
+      it "does not check the incorrect answers" do
+        expect(@checkbox_question.view_completed_question(0,0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_1_iscorrect" value="1" />')
+        expect(@checkbox_question.view_completed_question(0,0)).to include('id="quiz_question_choices_2_MultipleChoiceCheckbox_3_iscorrect" value="1" />')
+      end
+    end
+
+    context "when the question is true/false" do
+      it "contains the text of the question within a textarea tag" do
+        expect(@true_false_question.view_completed_question(0,0)).to include("2+2=4</textarea>")
+      end
+
+      it "contains each of the choices" do
+        expect(@true_false_question.view_completed_question(0,0)).to include("True")
+        expect(@true_false_question.view_completed_question(0,0)).to include("False")
+      end
+
+      it "puts each choice in a radio button" do
+        expect(@true_false_question.view_completed_question(0,0).scan('input type="radio"').size).to eq(2)
+      end
+
+=begin
+      it "selects only the correct answer" do
+        expect(@true_false_question.view_completed_question(0,0)).to include('value="True" checked="checked"')
+      end
+=end
+
+      it "does not select the incorrect answer" do
+        expect(@true_false_question.view_completed_question(0,0)).to include('value="False" />')
+      end
+    end
+  end
 end
